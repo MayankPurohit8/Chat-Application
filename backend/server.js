c0e369acf3d3f282c4918ab05adbb48f693aa40d
MayankPurohit8 <mayankpurohit888@gmail.com>
2025-08-03 11:16:03 +0530

    build-config

--- backend/server.js
+++ backend/server.js
@@ -28,5 +28,5 @@ app.get("/", (req, res) => {
 
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 server.listen(PORT);
